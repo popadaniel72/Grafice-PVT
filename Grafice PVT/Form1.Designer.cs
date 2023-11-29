e9a168fba2c77f099882330137c7af577bc1e872
popadaniel72 <popadaniel72@yahoo.com>
2023-11-29 11:34:56 +0200

    Adaugat izocora

--- Grafice PVT/Form1.Designer.cs
+++ Grafice PVT/Form1.Designer.cs
@@ -35,83 +35,108 @@ namespace Grafice_PVT
             this.button4 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
             this.button6 = new System.Windows.Forms.Button();
+            this.PVT = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(105, 12);
+            this.button1.Location = new System.Drawing.Point(140, 15);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(100, 28);
             this.button1.TabIndex = 0;
             this.button1.Text = "Izobara";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
-            this.button1.Paint += new System.Windows.Forms.PaintEventHandler(this.button1_Paint);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(200, 12);
+            this.button2.Location = new System.Drawing.Point(267, 15);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(100, 28);
             this.button2.TabIndex = 1;
             this.button2.Text = "Izocora";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(300, 12);
+            this.button3.Location = new System.Drawing.Point(400, 15);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.Size = new System.Drawing.Size(100, 28);
             this.button3.TabIndex = 2;
             this.button3.Text = "Izoterma";
             this.button3.UseVisualStyleBackColor = true;
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(405, 12);
+            this.button4.Location = new System.Drawing.Point(540, 15);
+            this.button4.Margin = new System.Windows.Forms.Padding(4);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.Size = new System.Drawing.Size(100, 28);
             this.button4.TabIndex = 3;
             this.button4.Text = "Adiabata";
             this.button4.UseVisualStyleBackColor = true;
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(515, 12);
+            this.button5.Location = new System.Drawing.Point(687, 15);
+            this.button5.Margin = new System.Windows.Forms.Padding(4);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.Size = new System.Drawing.Size(100, 28);
             this.button5.TabIndex = 4;
             this.button5.Text = "Ciclu";
             this.button5.UseVisualStyleBackColor = true;
             // 
             // button6
             // 
-            this.button6.Location = new System.Drawing.Point(842, 12);
+            this.button6.Location = new System.Drawing.Point(1123, 15);
+            this.button6.Margin = new System.Windows.Forms.Padding(4);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.Size = new System.Drawing.Size(100, 28);
             this.button6.TabIndex = 5;
             this.button6.Text = "Sterge";
             this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // PVT
+            // 
+            this.PVT.AutoSize = true;
+            this.PVT.Location = new System.Drawing.Point(872, 21);
+            this.PVT.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.PVT.Name = "PVT";
+            this.PVT.Size = new System.Drawing.Size(45, 16);
+            this.PVT.TabIndex = 6;
+            this.PVT.Text = "label1";
+            this.PVT.Click += new System.EventHandler(this.label1_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(984, 561);
+            this.ClientSize = new System.Drawing.Size(1312, 690);
+            this.Controls.Add(this.PVT);
             this.Controls.Add(this.button6);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.MaximumSize = new System.Drawing.Size(1000, 600);
-            this.MinimumSize = new System.Drawing.Size(1000, 600);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.MaximumSize = new System.Drawing.Size(1328, 729);
+            this.MinimumSize = new System.Drawing.Size(1328, 729);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Transformari PVT";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -123,6 +148,7 @@ namespace Grafice_PVT
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button5;
         private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label PVT;
     }
 }
 
--- Grafice PVT/Form1.cs
+++ Grafice PVT/Form1.cs
@@ -12,14 +12,66 @@ namespace Grafice_PVT
 {
     public partial class Form1 : Form
     {
-        int x = 50, y = 550;
+        int x = 50, y = 550, p=0, v=0;
         Pen rosu, negru, verde, albastru, portocaliu, galben;
+        int[] izobare;
+        int nrizobare=0, nrizocore = 0, mx = 10;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            izobara = false; izocora = false; izoterma = false; adiabata = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            izobara = false; izocora = false; izoterma = false; adiabata = false;
+            if (nrizocore < mx) 
+            {
+                nrizocore++;
+                izocora = true;
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            nrizocore = 0;
+            nrizobare = 0;
+            this.Invalidate();
+        }
+
+        int[] izocore;
+        bool izobara=false, izocora = false, izoterma = false, adiabata = false;
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            p = y - e.Y;
+            v = e.X - x;
+            PVT.Text = "P= " + p.ToString() + " V= " + v.ToString();
+            if(izobara) izobare[nrizobare] = e.Y;
+            if (izocora) izocore[nrizocore] = e.X;
+            if(izobara || izocora || izoterma || adiabata)
+            this.Invalidate();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
 
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
             /// desen grafic
             dVector(x, y, 90, 500, negru, e);
             dVector(x, y, 0, 800, negru, e);
+            for (int i = 1; i <= nrizobare; i++)
+                dIzobara(izobare[i], albastru, e);
+            for(int i=1; i<=nrizocore; i++)
+                dIzocora(izocore[i], verde, e);
         }
 
         
@@ -27,13 +79,24 @@ namespace Grafice_PVT
         public Form1()
         {
             InitializeComponent();
-            negru = new Pen(Color.Black, 1);
+            negru = new Pen(Color.Black, 2);
             rosu = new Pen(Color.Red, 1);
             verde = new Pen(Color.Green, 1);
             albastru = new Pen(Color.Blue, 1);
             galben = new Pen(Color.Yellow, 1);
-            portocaliu = new Pen(Color.Orange, 1); 
+            portocaliu = new Pen(Color.Orange, 1);
+            izobare = new int[mx + 1];
+            izocore = new int[mx + 1];
 
+        }
+
+        void dIzobara(int p, Pen cul, PaintEventArgs e)
+        {
+            e.Graphics.DrawLine(cul, x + 10, p, x + 500, p);
+        }
+        void dIzocora(int p, Pen cul, PaintEventArgs e)
+        {
+            e.Graphics.DrawLine(cul, p, y , p, 50);
         }
         void dVector(int x, int y, int u, int l, Pen cul, PaintEventArgs e)
         {
@@ -56,13 +119,16 @@ namespace Grafice_PVT
 
         }
 
-        private void button1_Paint(object sender, PaintEventArgs e)
-        {
-
-        }
+       
 
         private void button1_Click(object sender, EventArgs e)
         {
+            izobara = false; izocora = false; izoterma = false; adiabata = false;
+            if (nrizobare < mx)
+            {
+                izobara = true;
+                nrizobare++;
+            }
 
         }
     }
